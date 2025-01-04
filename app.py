18637c7b923ea6dab2970f27bf121073ecc2596c
D-Komil <tajhope@gmail.com>
2025-01-04 00:03:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ to explore some properties of a used car dataset
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import numpy as np
 
 df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
