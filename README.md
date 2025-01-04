99f3b67683fcdfc696dea267ce08643594267c96
D-Komil <tajhope@gmail.com>
2025-01-03 20:26:19 -0700

    final version of project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # sprint4project
 test project for sprint 4
+
+In this test app for sprint4 project 
+Using car sales dataset, 
+were plotted opportunity to compare
+cars' condition by milage.
+Car filter was added to filter by:
+        - manufacturer
+        - year
+        - price
+        - milage
---
+++ app.py
@@ -0,0 +1,53 @@
+'''
+In this project, we'll build a simple application 
+to explore some properties of a used car dataset
+
+'''
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('vehicles_us.csv')
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+
+
+# create a text header above the dataframe
+st.header('Car Sales Advertisements') 
+
+# display the dataframe with streamlit
+st.dataframe(df)
+
+st.header('Car Condition by Milage')
+
+# create a plotly histogram figure
+fig = px.histogram(df, x='odometer', color='condition')
+
+# display the figure with streamlit
+st.write(fig)
+
+st.header('Model condition by Year')
+fig = px.histogram(df, x='model_year', color='condition')
+st.write(fig)
+
+# Filters
+manufacturer_filter = st.multiselect(
+    'Select Manufacturer', options=df['manufacturer'].unique(), default=df['manufacturer'].unique()
+)
+min_year, max_year = st.slider('Select Model Year Range', min_value=int(df['model_year'].min()), max_value=int(df['model_year'].max()), value=(int(df['model_year'].min()), int(df['model_year'].max())))
+price_filter = st.slider('Select Price Range', min_value=int(df['price'].min()), max_value=int(df['price'].max()), value=(int(df['price'].min()), int(df['price'].max())))
+odometer_filter = st.slider('Select Milage Range', min_value=int(df['odometer'].min()), max_value=int(df['odometer'].max()), value=(int(df['odometer'].min()), int(df['odometer'].max())))
+
+# Apply Filters
+filtered_df = df[
+    (df['manufacturer'].isin(manufacturer_filter)) &
+    (df['model_year'] >= min_year) &
+    (df['model_year'] <= max_year) &
+    (df['price'] >= price_filter[0]) &
+    (df['price'] <= price_filter[1]) &
+    (df['odometer'] >= odometer_filter[0]) &
+    (df['odometer'] <= odometer_filter[1])
+]
+
+# Display Filtered Table
+st.write("Filtered Table:")
+st.dataframe(filtered_df)
